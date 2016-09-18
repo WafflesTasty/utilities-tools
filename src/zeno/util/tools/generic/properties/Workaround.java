237c574170d7e4289bbcd575c6f21b25fa6f4cc2
Zeno <zenothedefiant@gmail.com>
2016-09-18 09:45:01 +0200

    Added the Workaround marker interface.

---
+++ src/zeno/util/tools/generic/properties/Workaround.java
@@ -0,0 +1,17 @@
+package zeno.util.tools.generic.properties;
+
+/**
+ * The {@code Workaround} interface is a marker interface for temporary issues.
+ * <br> Classes tagged with this interface contain some form of workaround due
+ * to bugs in external libraries and/or drivers.
+ * 
+ * @since Sep 18, 2016
+ * @author Zeno
+ * 
+ * @see Deprecated
+ */
+@Deprecated
+public interface Workaround
+{
+	// NOT APPLICABLE
+}
