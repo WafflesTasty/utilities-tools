0312435c0fd62ac670f52cd42fe6ba84c7033af2
Zeno <Zeno@Jade>
2018-07-16 04:59:52 +0200

    Added counter classes.

---
+++ src/zeno/util/tools/counters/LongCounter.java
@@ -0,0 +1,40 @@
+package zeno.util.tools.counters;
+
+import java.util.Iterator;
+
+import zeno.util.tools.primitives.Longs;
+
+/**
+ * The {@code LongCounter} class creates a counter for long values.
+ *
+ * @since May 5, 2016
+ * @author Zeno
+ * 
+ * 
+ * @see Iterator
+ * @see Long
+ */
+public class LongCounter implements Iterator<Long>
+{
+	private long next;
+	
+	/**
+	 * Creates a new {@code LongCounter}.
+	 */
+	public LongCounter()
+	{
+		next = 0;
+	}
+	
+	@Override
+	public boolean hasNext()
+	{
+		return next != Longs.MAX_VALUE;
+	}
+
+	@Override
+	public Long next()
+	{
+		return next++;
+	}
+}
