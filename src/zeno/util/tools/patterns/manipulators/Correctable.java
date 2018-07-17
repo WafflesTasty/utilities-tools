295b59207c8e76de570a1f9ebcaa74523ef864a9
Zeno <Zeno@Jade>
2018-07-17 04:26:56 +0200

    Rearranged project.

--- src/zeno/util/tools/patterns/manipulators/Correctable.java
+++ src/zeno/util/tools/patterns/manipulators/Correctable.java
@@ -1,23 +1,23 @@
 package zeno.util.tools.patterns.manipulators;
 
 /**
- * The {@code Updateable} interface defines an object that can be updated.
+ * The {@code Correctable} interface defines an object that can be updated.
  *
  * @author Zeno
  * @since Jul 6, 2018
  * @version 1.0
  */
-public interface Updateable
+public interface Correctable
 {
 	/**
-	 * Checks if a state update is needed in the {@code Updateable}.
+	 * Checks if a state update is needed in the {@code Correctable}.
 	 * 
 	 * @return  {@code true} if an update is needed
 	 */
 	public abstract boolean needsUpdate();	
 	
 	/**
-	 * Requests a state update for the {@code Updateable}.
+	 * Requests a state update for the {@code Correctable}.
 	 */
 	public abstract void requestUpdate();
 }
