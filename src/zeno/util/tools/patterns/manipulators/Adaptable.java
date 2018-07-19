b4a9cfb6b9c3b756029f1af165f5f974513a1243
X-Zeno-X <zenothedefiant@gmail.com>
2018-07-19 05:36:05 +0200

    Rearranged project packages.

--- src/zeno/util/tools/patterns/manipulators/Adaptable.java
+++ src/zeno/util/tools/patterns/manipulators/Adaptable.java
@@ -1,23 +1,23 @@
 package zeno.util.tools.patterns.manipulators;
 
 /**
- * The {@code Correctable} interface defines an object that can be updated.
+ * The {@code Adaptable} interface defines an object that can be updated.
  *
  * @author Zeno
  * @since Jul 6, 2018
  * @version 1.0
  */
-public interface Correctable
+public interface Adaptable
 {
 	/**
-	 * Checks if a state update is needed in the {@code Correctable}.
+	 * Checks if a state update is needed in the {@code Adaptable}.
 	 * 
 	 * @return  {@code true} if an update is needed
 	 */
 	public abstract boolean needsUpdate();	
 	
 	/**
-	 * Requests a state update for the {@code Correctable}.
+	 * Requests a state update for the {@code Adaptable}.
 	 */
 	public abstract void requestUpdate();
 }
