9fc3104a84e46881119bf297c56d98ac660d1b99
X-Zeno-X <zenothedefiant@gmail.com>
2020-02-19 02:40:34 +0100

    Added the Resettable interface.

---
+++ src/zeno/util/tools/patterns/manipulators/Resettable.java
@@ -0,0 +1,16 @@
+package zeno.util.tools.patterns.manipulators;
+
+/**
+ * The {@code Resettable} interface defines an object capable of being reset.
+ *
+ * @author Zeno
+ * @since 19 Feb 2020
+ * @version 1.0
+ */
+public interface Resettable
+{
+	/**
+	 * Resets the state of the {@code Resettable}.
+	 */
+	public abstract void reset();
+}
