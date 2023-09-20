518f49bc61bb34bfeaf99cf0f19851f40943484b
WafflesTasty <zenothedefiant@gmail.com>
2023-09-20 16:20:43 +0200

    Added Ordered interface 1.1.

---
+++ src/waffles/utils/tools/patterns/semantics/Ordered.java
@@ -0,0 +1,18 @@
+package waffles.utils.tools.patterns.semantics;
+
+/**
+ * The {@code Ordered} interface defines an object with an order.
+ *
+ * @author Waffles
+ * @since 20 Sep 2023
+ * @version 1.1
+ */
+public interface Ordered
+{
+	/**
+	 * Returns the order of the object.
+	 * 
+	 * @return  an integer order
+	 */
+	public abstract int Order();
+}
