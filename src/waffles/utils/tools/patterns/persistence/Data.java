23bce7fbccbd3da731dbdcd274804139173cad9a
WafflesTasty <zenothedefiant@gmail.com>
2023-11-18 15:02:37 +0100

    Added Persistible pattern update 1.1.

---
+++ src/waffles/utils/tools/patterns/persistence/Data.java
@@ -0,0 +1,19 @@
+package waffles.utils.tools.patterns.persistence;
+
+/**
+ * The {@code Data} interface defines an abstract unit of data.
+ *
+ * @author Waffles
+ * @since 13 Nov 2023
+ * @version 1.1
+ */
+@FunctionalInterface
+public interface Data
+{
+	/**
+	 * Returns the size of the {@code Data}.
+	 * 
+	 * @return  a data size
+	 */
+	public abstract int DataSize();
+}
