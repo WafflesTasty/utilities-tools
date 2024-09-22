173db6aa84154d3d47f4e3c6feb9c97ff59c3d24
WafflesTasty <zenothedefiant@gmail.com>
2024-09-22 11:38:16 +0200

    Added Idleable interface, update 1.1.

---
+++ src/waffles/utils/tools/patterns/semantics/Idleable.java
@@ -0,0 +1,19 @@
+package waffles.utils.tools.patterns.semantics;
+
+/**
+ * The {@code Idleable} interface defines an object capable of idling.
+ *
+ * @author Waffles
+ * @since 22 Sep 2024
+ * @version 1.1
+ */
+@FunctionalInterface
+public interface Idleable
+{
+	/**
+	 * Checks if the {@code Idleable} is idle.
+	 * 
+	 * @return  {@code true} if idling
+	 */
+	public abstract boolean isIdle();
+}
