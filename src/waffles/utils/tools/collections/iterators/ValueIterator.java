5c19a3b6d2cec46ec6217200682005eec5e089fb
WafflesTasty <zenothedefiant@gmail.com>
2024-09-28 13:00:33 +0200

    Added ValueIterator, update 1.1.

---
+++ src/waffles/utils/tools/collections/iterators/ValueIterator.java
@@ -0,0 +1,47 @@
+package waffles.utils.tools.collections.iterators;
+
+import java.util.Iterator;
+
+import waffles.utils.tools.patterns.semantics.Valuable;
+
+/**
+ * A {@code ValueIterator} iterates over the values of a set of {@code Valuables}.
+ *
+ * @author Waffles
+ * @since 28 Sep 2024
+ * @version 1.1
+ *
+ *
+ * @param <V>  a value type
+ */
+public class ValueIterator<V> implements Iterator<V>
+{
+	private Iterator<Valuable<V>> source;
+	
+	/**
+	 * Creates a new {@code ValueIterator}.
+	 * 
+	 * @param src  a valuable source
+	 * 
+	 * 
+	 * @see Iterable
+	 * @see Valuable
+	 */
+	public ValueIterator(Iterable<Valuable<V>> src)
+	{
+		source = src.iterator();
+	}
+
+	
+	@Override
+	public boolean hasNext()
+	{
+		return source.hasNext();
+	}
+
+	@Override
+	public V next()
+	{
+		return source.next().Value();
+	}
+}
