b6f9bece0cdfdb8197b543f4746422a8b4bdfa11
WafflesTasty <zenothedefiant@gmail.com>
2025-08-14 13:11:59 +0200

    Removed BugFix from NotImplementedError, update 1.1.

--- src/waffles/utils/tools/patterns/basic/errors/NotImplementedError.java
+++ src/waffles/utils/tools/patterns/basic/errors/NotImplementedError.java
@@ -1,7 +1,5 @@
 package waffles.utils.tools.patterns.basic.errors;
 
-import waffles.utils.tools.patterns.BugFix;
-
 /**
  * A {@code NotImplementedError} is defined as a {@code RuntimeException}
  * which can be thrown in methods that still require implementation.
@@ -12,10 +10,8 @@ import waffles.utils.tools.patterns.BugFix;
  * 
  * 
  * @see RuntimeException
- * @see BugFix
  */
-@Deprecated
-public class NotImplementedError extends RuntimeException implements BugFix
+public class NotImplementedError extends RuntimeException
 {
 	private static final long serialVersionUID = -3471529381563386480L;
 }
